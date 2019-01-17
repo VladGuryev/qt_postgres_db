bea8a31928eac0b82bf7ae2c55f246fb57ab53a8
Vlad Guryev <teylor-world@yandex.ru>
2019-01-18 01:52:27 +0300

    work with db is configured

--- main.cpp
+++ main.cpp
@@ -9,13 +9,20 @@
 //https://ru.opensuse.org/QT_%D0%B8_%D0%91%D0%B0%D0%B7%D1%8B_%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85
 //https://habr.com/en/post/51650/
 
+//psql -d test -U postgres -h localhost -p 5433
+//conninfo
+// \c database;
+// \dt
+//  QString lCSql2 = QString("SELECT current_database();");
+// QString lCSql2 = QString("SELECT * FROM current_catalog;");
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
   QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
   db.setHostName("localhost");
-  db.setPort(5432);
+  db.setPort(5433); //на разных портах разные базы c одинаковым названием, лолчто
   db.setDatabaseName("test");
   db.setUserName("postgres");
   db.setPassword("postgres");
@@ -25,25 +32,26 @@ int main(int argc, char *argv[])
   } else{
     qDebug() << db.lastError().text() << endl;
   }
-  QSqlQuery query(db), query2(db);
-  //DON'T call prepare() with no binding values
+  QSqlQuery query(db);   //DON'T call prepare() with no binding values
+  QString query_string = QString("SELECT table_name "
+                                 "FROM information_schema.tables "
+                                 "WHERE table_schema "
+                                 "NOT IN ('information_schema','pg_catalog');");
 
-  QString lCSql = QString("SELECT table_name FROM information_schema.tables WHERE table_schema NOT IN ('information_schema','pg_catalog');");
+  query.exec(query_string);
 
- // query.exec(lCSql);
-
-  QString lCSql2 = QString("SELECT version();");
-  query2.exec(lCSql2);
+  query_string = QString("SELECT datname FROM pg_database;");
+  query_string = QString("SELECT * FROM courses;");
+  query.exec(query_string);
 
   QSqlError error = query.lastError();
   if (error.type() == QSqlError::NoError) {
-      while(query2.next()) {
-       qDebug() << query2.value(0).toString();
+      while(query.next()) {
+       qDebug() << query.value(0).toString();
       }
   } else {
       qDebug() << error.text();
   }
 
   return 0;
-
 }
