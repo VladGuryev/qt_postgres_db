7ab1f0aae1de5b1f15889dac37947c165e6f7281
Vlad Guryev <teylor-world@yandex.ru>
2019-01-22 23:26:33 +0300

    Some minor updates

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 //https://habr.com/en/post/128836/
 //https://ru.opensuse.org/QT_%D0%B8_%D0%91%D0%B0%D0%B7%D1%8B_%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85
 //https://habr.com/en/post/51650/
+//https://habr.com/en/post/128836/   очень неплохо описано
 
 //psql -d test -U postgres -h localhost -p 5433
 //conninfo
